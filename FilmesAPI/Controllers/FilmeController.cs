4f26ae3b7a02cf90a7bc1b016ac839be9deb149e
Julinha <Julinha@DESKTOP-RATS2DS>
2021-09-21 19:45:57 -0300

    Add project files.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.DTO;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+        public FilmeController(FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        //verbo HttpPost é padrão para criar dados via API
+        [HttpPost]
+        //Colocando o FromBody vc está dizendo que o filme vem do corpo (através do xml) da requisição
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDTO filmeDTO)
+        {
+            Filme filme = _mapper.Map<Filme>(filmeDTO);
+
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            Console.WriteLine(filme);
+            return CreatedAtAction(nameof(RecuperarFilmesPorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return _context.Filmes;
+        }
+
+        //para diferenciar o HttpGet do RecuperarFilmes e esse abaixo
+        //é necessário explicitar o parametro que é chamado pelo RecuperarFilmesPorId
+        [HttpGet("{id}")]
+        public IActionResult RecuperarFilmesPorId(int id)
+        {
+            //firstordefault retorna o primeiro valor que ele achar referente ao filme.Id, se não ele retorna nulo
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null)
+            {
+                ReadFilmeDTO filmeDTO = _mapper.Map<ReadFilmeDTO>(filme);
+            }
+            return NotFound();
+        }
+
+        //Verbo para atualizações
+        [HttpPut("{id}")]
+        public IActionResult AtualizarFilme(int id, [FromBody] UpdateFilmeDTO filmeDTO)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(filmeDTO, filme);
+            _context.SaveChanges();
+            //boa pratica de um put, é não retornar informação nenhuma, retorna um sem conteudo
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
